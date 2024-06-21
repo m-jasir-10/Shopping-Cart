f2123c355888d591614ee618c4d0150077b05d76
mjasir-10 <jasirmm307@gmail.com>
2024-06-21 23:06:40 +0600

    delete and edit btn interactive , added add product msg in admin

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -40,5 +40,39 @@ module.exports = {
                 reject(err);
             }
         });
+    },
+
+    deleteProduct: (productId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collections.PRODUCT_COLLECTION).deleteOne({ _id: new ObjectId(productId) })
+                .then((response) => {
+                    resolve(response);
+                });
+        });
+    },
+
+    getProductDetails: (productId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collections.PRODUCT_COLLECTION).findOne({ _id: new ObjectId(productId) })
+                .then((product) => {
+                    resolve(product);
+                });
+        });
+    },
+
+    updateProduct: (productId, product) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collections.PRODUCT_COLLECTION)
+                .updateOne({ _id: new ObjectId(productId) }, {
+                    $set: {
+                        name: product.name,
+                        description: product.description,
+                        price: product.price,
+                        category: product.category
+                    }
+                }).then((response) => {
+                    resolve();
+                });
+        });
     }
 };
